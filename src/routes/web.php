40e47677efe2ff0f4303016bc7c256f2b87501ba
Tiago <collinpool2012@gmail.com>
2026-04-17 17:06:25 -0300

    Correção dos caminhos controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\CardapioController;
+use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PedidosController;
+use App\Http\Controllers\RegiaoController;
+use App\Http\Controllers\SobreController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/', [SobreController::class, 'sobre'])->name('sobre');
-Route::get('/', [CardapioController::class, 'cardapio'])->name('cardapio');
-Route::get('/', [PedidosController::class, 'pedidos'])->name('pedidos');
-Route::get('/', [RegiaoController::class, 'regiao'])->name('regiao');
-Route::get('/', [ContatoController::class, 'contato'])->name('contato');
+Route::get('/sobre', [SobreController::class, 'sobre'])->name('sobre');
+Route::get('/cardapio', [CardapioController::class, 'cardapio'])->name('cardapio');
+Route::get('/pedidos', [PedidosController::class, 'pedidos'])->name('pedidos');
+Route::get('/regiao', [RegiaoController::class, 'regiao'])->name('regiao');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('contato');
