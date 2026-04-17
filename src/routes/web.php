956f5458f412facb83eae5806d8bcd23795055ce
Tiago <collinpool2012@gmail.com>
2026-04-17 12:08:16 -0300

    páginas

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\CardapioController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('site.home');
-});
 
-
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/', [SobreController::class, 'sobre'])->name('sobre');
+Route::get('/', [CardapioController::class, 'cardapio'])->name('cardapio');
+Route::get('/', [PedidosController::class, 'pedidos'])->name('pedidos');
+Route::get('/', [RegiaoController::class, 'regiao'])->name('regiao');
+Route::get('/', [ContatoController::class, 'contato'])->name('contato');
